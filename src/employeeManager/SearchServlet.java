4310b6b17ed31dbbf4cca5e1055b555891a6435e
csaszarhunor <csaszarhunor@gmail.com>
2016-03-16 17:31:30 +0100

    phew

--- src/employeeManager/SearchServlet.java
+++ src/employeeManager/SearchServlet.java
@@ -2,18 +2,17 @@ package employeeManager;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(description = "Employee Manager Search Servlet", urlPatterns = { "/SearchServlet" })
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		
 	}
 
 }
